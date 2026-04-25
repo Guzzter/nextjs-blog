1d03d36967829aa7d6f015430ec8efcd7477c7b5
Guus Beltman <guusbeltman@gmail.com>
2026-04-25 09:34:25 +0200

    error page

---
+++ apps/blog/src/app/error.tsx
@@ -0,0 +1,9 @@
+export default function ErrorBoundary() {
+  return (
+    <div className="max-w-6xl mx-auto px-4 py-10 sm:px-6 w-full">
+      <div className="flex flex-col items-center justify-center min-h-[50vh]">
+        <h1 className="font-bold mb-4 text-4xl">Server error</h1>
+      </div>
+    </div>
+  );
+}
